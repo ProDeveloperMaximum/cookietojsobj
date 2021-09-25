33c848b18b757ca609a44316856fdd160648c6c9
unknown <heseneli536@gmail.com>
2021-09-25 22:50:44 +0400

    new file:   .gitignore 	new file:   README.md 	new file:   cookietojsobj.js 	new file:   package-lock.json 	new file:   package.json

---
+++ README.md
@@ -0,0 +1,13 @@
+# Description
+NodeJS package that parses HTML cookie to Javascript object.
+# Installation
+```sh
+$ npm i cookietojsobj --save
+```
+# Example
+```
+const mod = require("cookietojsobj")
+
+mod.cookieToJSOBJ("name=heseneli;sname=kadirov")
+// output: {name:heseneli,sname:kadirov}
+```
---
+++ cookietojsobj.js
@@ -0,0 +1,17 @@
+module.exports.cookieToJSOBJ = (c) => {
+  let c2 = decodeURIComponent(c).split(";")
+  let res = ""
+  for (var i = 0; i < c2.length; i++){
+    c2[i] = c2[i].split("=")
+  }
+  for (var r = 0; r < c2.length; r++){
+    c2[r][0] = "\"" + c2[r][0] + "\""
+    c2[r][1] = "\"" + c2[r][1] + "\""
+    c2[r] = c2[r].join(":")
+    res += c2[r] + ","
+  }
+  return JSON.parse("{" + res.slice(0, res.length - 1) + "}")
+}
+module.exports.getValuebyKeyInCookie = (c, key) => {
+  return this.cookieToJSOBJ(c)[key]
+}
